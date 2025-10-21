ccc2e138015cb721d9ce1a2d25a37e132ee8694e
mohammedjacil272-gif <mohammedjacil272@gmail.com>
2025-10-21 19:26:29 +0530

    this is the final commit

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,17 +1,27 @@
+# Purpose Of this program is to arrive to a side of review(positive, negative or neutral) based on the csv file given as input  
+#import TextBlob to for NLP and sentiment scoring
+#import csv to deal with file functions 
 from textblob import TextBlob
-import csv
+import csv 
+#create a list to store all polarity scores
 all_polarities = []
+#open csv file to read its content
 with open ('breakingbad-positive-response.csv','r',newline='',encoding='utf-8') as data_file :
+    #this is used to reading data off the csv file 
     data_reader = csv.reader(data_file)
+    #read content and add polarity score to all_polarity list
     for para in data_reader :
+        #checks if para exists and skips empty para
         if para  and para[0].strip():
             analysis = TextBlob(para[0])
             polarity = analysis.sentiment.polarity
             all_polarities.append(polarity)
 i=len(all_polarities)
+#based on size of sample choose parameter to evaluate sentiment of text/review and also print result based on average polarity
 if i < 80 :
     if all_polarities :
         average_polarity = sum(all_polarities)/i
+        #parameter is 0.05 due to relatively small sample size
         if average_polarity > 0.05 :
             print(f'Polarity is {average_polarity}')
             print("It is relatively positive")
@@ -24,6 +34,7 @@ if i < 80 :
 if i >= 80 :
     if all_polarities :
         average_polarity = sum(all_polarities)/i
+        #parameter is 0.2 due to relatively large sample size
         if average_polarity > 0.2 :
             print(f'Polarity is {average_polarity}')
             print("It is relatively positive")
--- data_scrapper.py
+++ data_scrapper.py
@@ -1,31 +1,56 @@
+#Purpose : To find the data from review section of wikipedia/other website and write it in a csv file
+#import requests to send HTTP requests and download web pages
+#import BeautifulSoup to parse HTML and extract specific data
+#import time to add a pause between the requests sent to a website
+#import csv to deal with file functions
 import requests 
 from bs4 import BeautifulSoup
 import time 
 import csv
-web_url="https://en.wikipedia.org/wiki/Thor:_The_Dark_World"
+#create variable to store the website link
+web_url="https://en.wikipedia.org/wiki/The_Big_Bang_Theory"
+#create a variable to store agent data
 header_data={'User-agent':'EthicalScrapper/1.0'}
+#create a variable to store data obtained from get() that fetches page content
 web = requests.get(web_url,headers=header_data)
+# add a delay between requests to a page 
 time.sleep(3)
+#parse the information obtained and create a variable to store it
 soup = BeautifulSoup(web.text,'html.parser')
-target_element = soup.find(id='Critical_response')
-csv_file= open("thor-neutral-response.csv",'w',newline='', encoding='utf-8')
+#create a starting point 
+target_element = soup.find(id='American_ratings')
+#create and open the csv file to store the information that is going to be extracted
+csv_file= open("bigbangtheory-neutral-response.csv",'w',newline='', encoding='utf-8')
+#use csv.writer to write content
 csv_writer = csv.writer(csv_file)
+#create a row in csv file 
 csv_writer.writerow(['Paragraph'])
+#create a list to store the information/paragraphs extracted
 paragraphs=[]
+#find the parent of the targeted portion to begin
 start_point = target_element.parent
+#check if a starting point exists
 if start_point:
+    #find the next sibling/element that comes after the target
     current_point=start_point.find_next_sibling()
+    #create a loop to find a paragraph to extract 
     while current_point and current_point.text != 'h3':
+        #check if the paragraph is empty
         if current_point.name == 'p' and current_point.text.strip():
+            # add the paragraph to the list
             paragraphs.append(current_point.text.strip())
+        #find the next element    
         current_point = current_point.find_next_sibling()
+#check is the list is empty
 if paragraphs:
     print('Exraction succesful')
+    # add the contents from the list to the csv file using a loop
     for i, para in enumerate(paragraphs):
         csv_writer.writerow([para])
     print('Writing to file succesful')
     print(f'{len(paragraphs)} paragraphs obtained and written')
 else:
-    print("Extraction Unsuccesful") 
+    print("Extraction Unsuccesful")
+#close the opened csv file
 csv_file.close()
 
